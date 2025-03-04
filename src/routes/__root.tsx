2ef4af9a04e3a17fe9943e16679aa2e963ec92a3
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-03-04 00:30:05 -0800

    Remove fixed header and adjust layout in root route component

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,10 +4,7 @@ import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 export const Route = createRootRoute({
     component: () => (
         <>
-            <div className="fixed flex h-10 w-full items-center justify-center border-b bg-white font-bold">Peer Chat</div>
-            <div className="pt-10">
-                <Outlet />
-            </div>
+            <Outlet />
             <TanStackRouterDevtools />
         </>
     )
