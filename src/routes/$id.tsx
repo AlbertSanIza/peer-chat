9e18926648a237e4ac542354dd2124474de5ec53
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-03-04 00:45:05 -0800

    Replace room route with dynamic ID route for improved routing flexibility

---
+++ src/routes/$id.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/$id')({ component: RouteComponent })
+
+function RouteComponent() {
+    const { id } = Route.useParams()
+
+    return <div>Hello "/{id}"!</div>
+}
--- src/routes/room.$roomId.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/room/$roomId')({ component: RouteComponent })
-
-function RouteComponent() {
-    const { roomId } = Route.useParams()
-
-    return <div>Hello "/room/{roomId}"!</div>
-}
