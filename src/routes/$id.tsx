9d842365fdb2df3d6242ec885adc0cc49afffd17
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-03-04 19:51:16 -0800

    Enhance room route component to include ChatRoom and PeerProvider for improved functionality

--- src/routes/$id.tsx
+++ src/routes/$id.tsx
@@ -1,9 +1,18 @@
 import { createFileRoute } from '@tanstack/react-router'
 
+import { ChatRoom } from '../components/ChatRoom'
+import { PeerProvider } from '../context/PeerContext'
+
 export const Route = createFileRoute('/$id')({ component: RouteComponent })
 
 function RouteComponent() {
     const { id } = Route.useParams()
 
-    return <div>Hello "/{id}"!</div>
+    return (
+        <div className="h-screen">
+            <PeerProvider peerId={id}>
+                <ChatRoom roomId={id} />
+            </PeerProvider>
+        </div>
+    )
 }
