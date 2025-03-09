280b95a29351f4c530b302216302f90b788d22a4
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-03-09 14:57:45 -0700

    Refactor Header component to improve UI and enhance peer ID handling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,32 +1,31 @@
-import { LoaderIcon } from 'lucide-react'
 import { useState } from 'react'
 
 import { usePeer } from '../context/usePeer'
 
 export default function Header({ onConnect }: { onConnect: (peerId: string) => void }) {
-    const { peer, status } = usePeer()
+    const { peer } = usePeer()
     const [peerId, setPeerId] = useState('')
 
     const handleConnect = () => {
         if (!peerId.trim()) {
             return
         }
-        onConnect(peerId.trim())
+        onConnect(peerId.trim().toLowerCase())
     }
 
     return (
-        <div className="border-b border-gray-300 p-4">
-            {status.loading ? (
-                <LoaderIcon className="animate-spin" />
-            ) : (
-                <div className="flex flex-col items-center justify-center gap-4">
-                    <div>ID: {peer?.id}</div>
-                    <input className="border border-gray-300" placeholder="Enter Peer ID" onChange={(event) => setPeerId(event.target.value)} />
-                    <button className="rounded-md bg-blue-500 p-1 text-white" onClick={handleConnect}>
-                        Connect
-                    </button>
-                </div>
-            )}
+        <div className="flex items-center justify-center border-b border-gray-300 p-4">
+            <div className="flex flex-col items-center justify-center gap-4">
+                <div>My ID: {peer?.id.toUpperCase()}</div>
+                <input className="border border-gray-300" placeholder="Enter Peer ID" onChange={(event) => setPeerId(event.target.value)} />
+                <button
+                    className="cursor-pointer rounded-md bg-blue-500 p-1 text-white disabled:cursor-default disabled:opacity-50"
+                    onClick={handleConnect}
+                    disabled={!peerId.trim()}
+                >
+                    Connect
+                </button>
+            </div>
         </div>
     )
 }
