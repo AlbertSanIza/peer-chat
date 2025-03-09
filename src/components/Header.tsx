0532b9069ed0072bcb63eac2087566f47934c917
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-03-09 15:41:24 -0700

    Refactor Header component layout for improved button grouping and spacing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -20,27 +20,29 @@ export default function Header({ onConnect }: { onConnect: (peerId: string) => v
             <div className="size-20 min-w-20">
                 <QRCode value={peer?.id || 'loading'} style={{ height: 'auto', maxWidth: '100%', width: '100%' }} />
             </div>
-            <div className="flex flex-1 flex-col gap-2">
+            <div className="flex flex-1 flex-col justify-between gap-2">
                 <div className="flex justify-between gap-4 text-sm font-bold sm:text-lg">
                     {peer?.id.toUpperCase()}
                     <div className="size-3 min-w-3 animate-pulse rounded-full bg-red-500"></div>
                 </div>
-                <div className="flex flex-col items-center gap-4 sm:flex-row">
+                <div className="flex flex-col items-center gap-2 sm:flex-row sm:gap-4">
                     <input
                         className="h-10 min-h-10 w-full rounded-sm border border-gray-300 px-2"
                         placeholder="Enter Peer ID"
                         onChange={(event) => setPeerId(event.target.value)}
                     />
-                    <button
-                        className="w-full cursor-pointer rounded-md bg-blue-500 p-2 text-white disabled:cursor-default disabled:opacity-50 sm:w-fit"
-                        onClick={handleConnect}
-                        disabled={!peerId.trim()}
-                    >
-                        Connect
-                    </button>
-                    <button className="cursor-pointer rounded-md bg-blue-500 p-2 text-white disabled:cursor-default disabled:opacity-50">
-                        <CameraIcon className="" />
-                    </button>
+                    <div className="flex gap-2">
+                        <button
+                            className="w-full cursor-pointer rounded-md bg-blue-500 p-2 text-white disabled:cursor-default disabled:opacity-50 sm:w-fit"
+                            onClick={handleConnect}
+                            disabled={!peerId.trim()}
+                        >
+                            Connect
+                        </button>
+                        <button className="cursor-pointer rounded-md bg-blue-500 p-2 text-white disabled:cursor-default disabled:opacity-50">
+                            <CameraIcon className="" />
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
