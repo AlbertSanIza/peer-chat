7326e2eb8e5718edecaf659595e8fd702ab0384b
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-03-09 14:01:33 -0700

    Refactor Footer component to handle message sending and improve input validation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,40 +1,30 @@
+import { SendHorizonalIcon } from 'lucide-react'
+import type { DataConnection } from 'peerjs'
 import { KeyboardEvent, useState } from 'react'
 
-import { SendHorizonalIcon } from 'lucide-react'
 import { usePeer } from '../context/usePeer'
 
-export default function Footer({ onSendMessage }) {
-    const { status } = usePeer()
+export default function Footer({ connection, onSendMessage }: { connection?: DataConnection; onSendMessage: (message: string) => void }) {
+    const { peer, status } = usePeer()
     const [message, setMessage] = useState('')
-
-    const sendMessage = () => {
-        if (!message.trim() || !connection) {
-            return
-        }
-        const messageToSend: IMessage = {
-            content: message,
-            sender: 'me',
-            timestamp: Date.now()
-        }
-        connection.send({ type: 'message', message: messageToSend })
-        setMessages((prevMessages) => [...prevMessages, messageToSend])
-        setMessage('')
-    }
-
-    const handleOnSendMessage = () => {
-        onSendMessage(message)
-        setMessage('')
-    }
 
     const handleKeyPress = (event: KeyboardEvent) => {
         if (event.key === 'Enter' && !event.shiftKey) {
             event.preventDefault()
-            sendMessage()
+            handleSendMessage()
         }
     }
 
+    const handleSendMessage = () => {
+        if (!message.trim()) {
+            return
+        }
+        onSendMessage(message)
+        setMessage('')
+    }
+
     return (
-        <div className="flex border-t border-gray-300 p-4">
+        <div className="flex border-t border-gray-300 bg-amber-50 p-4">
             <input
                 placeholder="Type a message..."
                 className="h-12 w-full rounded-3xl border border-gray-300 pr-11 pl-2.5 disabled:opacity-50"
@@ -43,7 +33,10 @@ export default function Footer({ onSendMessage }) {
                 onKeyDown={handleKeyPress}
                 onChange={(event) => setMessage(event.target.value)}
             />
-            <button className="absolute right-6 bottom-6 rounded-full bg-blue-500 p-2.5 text-white disabled:opacity-50" disabled={!message || !connectedToPeer}>
+            <button
+                className="absolute right-6 bottom-6 rounded-full bg-blue-500 p-2.5 text-white disabled:opacity-50"
+                disabled={!message.trim() || !status.online || !connection}
+            >
                 <SendHorizonalIcon className="size-3" />
             </button>
         </div>
