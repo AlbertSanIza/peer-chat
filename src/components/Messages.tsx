b9be5180521228df67e3dbe292071e788ce9bbc7
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-03-09 14:22:02 -0700

    Add Messages component to display and auto-scroll chat messages

---
+++ src/components/Messages.tsx
@@ -0,0 +1,22 @@
+import { useEffect, useRef } from 'react'
+
+import { IMessage, Message } from '../Message'
+
+export default function Messages({ messages }: { messages: IMessage[] }) {
+    const messagesEndRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' })
+    }, [messages])
+
+    return (
+        <div className="flex-1 overflow-hidden">
+            <div className="size-full overflow-auto pt-2">
+                {messages.map((message, index) => (
+                    <Message key={index} message={message} />
+                ))}
+                <div ref={messagesEndRef} />
+            </div>
+        </div>
+    )
+}
