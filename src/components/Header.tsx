44fb2e06ee121df9542d5785ccd9b7d760e6d4ab
Albert Sanchez <albert.sanchez@tcwglobal.com>
2025-03-09 15:36:08 -0700

    Enhance Header component layout by improving input width, button styling, and adding camera icon for scan functionality

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import QRCode from 'react-qr-code'
 
+import { CameraIcon } from 'lucide-react'
 import { usePeer } from '../context/usePeer'
 
 export default function Header({ onConnect }: { onConnect: (peerId: string) => void }) {
@@ -20,25 +21,25 @@ export default function Header({ onConnect }: { onConnect: (peerId: string) => v
                 <QRCode value={peer?.id || 'loading'} style={{ height: 'auto', maxWidth: '100%', width: '100%' }} />
             </div>
             <div className="flex flex-1 flex-col gap-2">
-                <div className="flex justify-between gap-4 font-bold">
+                <div className="flex justify-between gap-4 text-sm font-bold sm:text-lg">
                     {peer?.id.toUpperCase()}
                     <div className="size-3 min-w-3 animate-pulse rounded-full bg-red-500"></div>
                 </div>
                 <div className="flex flex-col items-center gap-4 sm:flex-row">
                     <input
-                        className="h-10 min-h-10 flex-1 rounded-sm border border-gray-300 px-2"
+                        className="h-10 min-h-10 w-full rounded-sm border border-gray-300 px-2"
                         placeholder="Enter Peer ID"
                         onChange={(event) => setPeerId(event.target.value)}
                     />
                     <button
-                        className="w-full cursor-pointer rounded-md bg-blue-500 p-2 text-white disabled:cursor-default disabled:opacity-50"
+                        className="w-full cursor-pointer rounded-md bg-blue-500 p-2 text-white disabled:cursor-default disabled:opacity-50 sm:w-fit"
                         onClick={handleConnect}
                         disabled={!peerId.trim()}
                     >
                         Connect
                     </button>
-                    <button className="w-full cursor-pointer rounded-md bg-blue-500 p-2 text-white disabled:cursor-default disabled:opacity-50">
-                        Scan to Connect
+                    <button className="cursor-pointer rounded-md bg-blue-500 p-2 text-white disabled:cursor-default disabled:opacity-50">
+                        <CameraIcon className="" />
                     </button>
                 </div>
             </div>
